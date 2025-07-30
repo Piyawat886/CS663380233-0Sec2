5128811027fcb47e94bc6e44827e4727d7ab47dd
witwin <witthawin.b@kkumail.com>
2025-07-30 16:49:37 +0700

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,3 +1,12 @@
 public class Department {
-    
+    private int id;
+    private String name;
+
+    public Department(){}
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
 }
